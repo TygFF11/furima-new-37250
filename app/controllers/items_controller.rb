a880246b7725928cef0cc66dfbab1f0feb98911c
TygFF11 <kh091216@icloud.com>
2022-06-12 20:34:31 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,29 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
 
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
+    @item = Item.new
   end
+
+  def create
+    # binding.pry
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name,:item_describe,:item_price, :category_id, :situation_id, :prefecture_id, :delivary_id, :day_id).merge(user_id: current_user.id)
+  end
+
   
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,25 @@
 class Item < ApplicationRecord
+  validates :item_name,         presence: true
+  validates :item_describe,     presence: true
+  validates :item_price,        
+            numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+  validates :image,             presence: true
+  
+  validates :category_id,       numericality: { other_than: 1 , message: "can't be blank" }
+  validates :situation_id,      numericality: { other_than: 1 , message: "can't be blank" }
+  validates :prefecture_id,     numericality: { other_than: 1 , message: "can't be blank" }
+  validates :delivary_id,       numericality: { other_than: 1 , message: "can't be blank" }
+  validates :day_id,            numericality: { other_than: 1 , message: "can't be blank" }
+  
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :situation
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :delivary
+  belongs_to_active_hash :day
+  
 end
